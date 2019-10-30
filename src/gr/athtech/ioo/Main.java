4b38436db7b2ad1fa6bc6cd8e159112414f92902
JimmydK <dKaramanidis@athtech.r>
2019-10-30 13:25:39 +0200

    Initial commit - only airports

--- src/gr/athtech/ioo/Main.java
+++ src/gr/athtech/ioo/Main.java
@@ -6,7 +6,9 @@ public class Main {
         Flight f1 = new Flight();
         f1.setDepAirport("Athens");
         f1.setArAirport("Callifornia");
-            System.out.println(f1);
+        System.out.println(f1);
+        System.out.println(f1.getArAirport());
+        System.out.println(f1.getDepAirport());
 
     }
 }
