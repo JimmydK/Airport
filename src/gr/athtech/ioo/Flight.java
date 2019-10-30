4a8cde42b400bc20fe5f679fcb220dd2d65a54e4
JimmydK <dKaramanidis@athtech.r>
2019-10-30 13:20:38 +0200

    Initial commit - only airports

---
+++ src/gr/athtech/ioo/Flight.java
@@ -0,0 +1,20 @@
+package gr.athtech.ioo;
+
+public class Flight {
+    private String arAirport;
+    private String depAirport;
+
+    public void setArAirport(String arAirport) { this.arAirport = arAirport;}
+    public void setDepAirport(String depAirport) { this.depAirport = depAirport;}
+
+    public String getArAirport(){
+        return arAirport;
+    }
+    public String getDepAirport(){
+        return depAirport;
+    }
+    public String toString(){
+        return "Flight - From:" + depAirport + ", To:" + arAirport;
+    }
+
+}
---
+++ src/gr/athtech/ioo/Main.java
@@ -0,0 +1,12 @@
+package gr.athtech.ioo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Flight f1 = new Flight();
+        f1.setDepAirport("Athens");
+        f1.setArAirport("Callifornia");
+            System.out.println(f1);
+
+    }
+}
